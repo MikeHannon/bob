c6ca04f3ff9a6603d733c50572cea52e57dd873c
Mike Hannon <m.hannon@legworkprm.com>
2023-07-15 07:58:03 -0700

    Initial commit:

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+import './App.css';
+import Welcome from './Welcome';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Welcome/>
+        <img className="App-logo" alt="Avery and Bob" src={"https://lh3.googleusercontent.com/pw/AIL4fc9QKx1tvzCXg4V7WW86IeBKRubpY2sBibReQnnLU4VxyGDy_rVxZGBcnipE2Wu6mVXuZ-RfmGDmyLU1mBQJU-TyiFrgc9ggShddUYYw-uasQAR3jKNP5RkjI_Tsq_eO53vlZL5HQ8D_W1myBFdpW8DCTCVjvcQeRgcEqYLzthzdGCzky17U_3lyrEXp9tN2tvStQ_O54zEqIQzo-8ssDiQxJvV0zLuBNZQ2Ot_YjUxEAV2lOSOTIb7D-M-XMS2C0xdItTNl7BBToMctpDhydp5vuCI52RK2c1EyKOfNXpcTLO-qdx3SrIleqHSNxLmfThXHHVRlXxVns1cZxQ7j7egGtNqur_Z0-vt5kdES3A-s2fs12yxaAq0hyyo4q2hn55C-JLcN78wuxkMrtXUWS2XELi2o5i5aSp7YUYqFMqdRr-ajwOpdkVrIqhTkbvWeB6p5KF_wdfw6EqtPPK6Wyie8QFUQGSUxs5vSHAM4Arp0ERll4mJyIJ7WDCYIX_pE0pWfREm2RAL9o8coyMD7QLf_cSutt2Xtv2xHvlHAVQJom29pRj-uGZ-R41Ypi6X6I40yANxEvFCV3TIyaipPJUJyMTYO2K2_QvZpWWB8NNWBa9yWnnm-YI3rVtucV7bZAm2ktEIeOPUYoRJsXmDidfQJ6YSaaVO-CYXzq_3BkOCvLVe8tUKIdEn6G1lGYTa4XAUeN2bnxGbgI-CJEtYbSy_-3xRznNtFDT_WrDLDahcQL3GjHGLglGL12TMwoiGqGx057eGr-TH4dLgha9VjHdbuVMDGDSkCs6MgnNr8vz4Eajur7ReqhVSKAwINq__S4QF1cdu9opabW2oc1VMLuiz1lZBj567IDncEnF2doEIKi2yfk94WcF4MgGMUZvKlBNXpidr0jl8kJWqb-vwzU9pLow=w1256-h1674-s-no?authuser=0"}/>
+        
+      
+      <p> Walk these mountains my friend. </p>
+
+      <img src={"https://sepwww.stanford.edu/sep/morgan/images/scenery/yosemite/tresidder/tp21-cathedral-peak-lake-pano.jpg"} className="App-logo" alt="Yosemite" />
+      <p> I thought I had more time, but I will never regret the time I had. </p>
+
+      <p> Ann, Jenny, Doug - Much love to all of you.  And thank you, Bob has a huge presence in my life. </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Welcome.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Welcome = () => {
+  return <div>
+    <h1> Bob </h1>
+
+    <p> Thanks for everything.  
+    </p>
+    
+
+
+  </div>
+}
+
+export default Welcome;
